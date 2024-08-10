8ae58a0196190752145d30431fecefc8cb9cc502
HawraaYounes <hawraayounes0@gmail.com>
2024-08-10 14:22:47 +0300

    finished contact section functionality

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -15,8 +15,47 @@ const Contact = () => {
     message: "",
   });
 
-  const handleChange = (e) => {};
-  const handleSubmit = (e) => {};
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    emailjs
+      .send(
+        "service_ms6jssr",
+        "template_yyl43gl",
+        {
+          from_name: form.name,
+          to_name: "Hawraa",
+          from_email: form.email,
+          to_email: "hawraayounes0@gmail.com",
+          message: form.message,
+        },
+        "H7xmfpq0rPZAK1hw0"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert(
+            `Thank you ${form.name}. I will get back to you as soon as possible!`
+          );
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.log(error);
+          alert("Something went wrong!");
+        }
+      );
+  };
 
   return (
     <div className="xl:mt-12 xl:flex-row  flex-col-reverse flex gap-10 overflow-hidden">
@@ -64,12 +103,20 @@ const Contact = () => {
               className="bg-tertiary py-4 px-6 text-white rounded-lg outline-none border-none font-medium placeholder:text-secondary"
             />
           </label>
-          <button 
-          type="submit"
-          className="bg-[#915eff] py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-violet-700 rounded-xl">
-            {loading? 'Sending...': 'Send'}
+          <button
+            type="submit"
+            className="bg-[#915eff] py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-violet-700 rounded-xl"
+          >
+            {loading ? "Sending..." : "Send"}
           </button>
         </form>
+      </motion.div>
+
+      <motion.div
+        variants={slideIn("right", "tween", 0.2, 1)}
+        className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"
+      >
+        <EarthCanvas />
       </motion.div>
     </div>
   );
