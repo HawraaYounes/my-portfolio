31b32d543e7fc8c1f78d62570add1cab4e7fb8e4
HawraaYounes <hawraayounes0@gmail.com>
2024-08-11 11:15:50 +0300

    updated tech component

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 
 const Tech = () => {
+  const scrollContainerRef = useRef(null);
+
+  useEffect(() => {
+    const scrollContainer = scrollContainerRef.current;
+
+    const handleScroll = () => {
+      if (scrollContainer.scrollLeft >= scrollContainer.scrollWidth / 2) {
+        scrollContainer.scrollLeft = 0;
+      }
+    };
+
+    scrollContainer.addEventListener("scroll", handleScroll);
+
+    return () => {
+      scrollContainer.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <div className='overflow-x-auto whitespace-nowrap py-10 scrollbar-hidden'>
-      <div className='inline-flex gap-10'>
-        {technologies.map((technology) => (
-          <div className='w-28 h-28 flex-shrink-0' key={technology.name}>
-            <img src={technology.icon} alt={technology.name} className='w-full h-full object-contain' />
+    <div className="scroll-container" ref={scrollContainerRef}>
+      <div className="scroll-content">
+        {[...technologies, ...technologies].map((technology, index) => (
+          <div className="w-28 h-28 flex-shrink-0 mx-3" key={index}>
+            <img src={technology.icon} alt={technology.name} className="w-full h-full object-contain " />
           </div>
         ))}
       </div>
